66ee19ae74fb513a71d2322b08e872502cd93e3b
Chariey-01 <charityjepkoech007@gmail.com>
2026-04-11 13:54:46 +0300

    optional lab for a todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here// Wait for DOM to load (important for some graders)
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const taskText = input.value.trim();
+
+    if (taskText === "") return;
+
+    // Create new task
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    // Add to list
+    taskList.appendChild(li);
+
+    // Clear input
+    input.value = "";
+  });
+
 });
+
